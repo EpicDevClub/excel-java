27729b0adc0560f99367b0abdf365ddecb9f6f39
Qamar Memon <93121057+Qambho@users.noreply.github.com>
2021-10-25 13:51:31 +0500

    Create PersonExample.java

---
+++ OOP Basics/PersonExample.java
@@ -0,0 +1,19 @@
+class Person {
+  int age, weight;
+  String name;
+  
+  public void eat() {}
+  public void walk() {}
+}
+
+class Police extends Person {
+  int level;
+  
+  public void arrest() {}
+}
+
+class Teacher extends Person {
+  int bps;
+  
+  public void teach() {}
+}
